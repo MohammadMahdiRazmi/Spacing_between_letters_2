8fca3761f4e25b2923de49fa96b0163cdebc477f
MahdiRazmi <109846619+MohammadMahdiRazmi@users.noreply.github.com>
2023-08-06 22:21:05 +0330

    project

---
+++ project - Copy.py
@@ -0,0 +1,9 @@
+print("wellcome to my app----->")
+name = input("Please Enter Name:")
+separated_name = tuple(name)
+
+print("Noun not attached:")
+print(separated_name)
+
+print("Noun not attached:")
+print(''.join(separated_name))
